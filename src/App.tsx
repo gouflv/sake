16d3fb739f6befe3a6504d86bb58d0c8456b3c58
gouflv <lv.gouf@gmail.com>
2020-12-01 22:52:19 +0800

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
