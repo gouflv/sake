1210285671c2874fd218769caf8fa347e270d16b
gouflv <lv.gouf@gmail.com>
2020-12-02 18:52:49 +0800

    [WIP] optimize: render performance

--- src/components/Message/plugins/Notification.tsx
+++ src/components/Message/plugins/Notification.tsx
@@ -1,13 +1,14 @@
-import {FC} from 'react'
+import {FC, memo} from 'react'
 import styled from 'styled-components'
 import {MessageComponentProps} from '../../../typing'
 
 const Notification = styled.div`
+  margin: 0.25rem;
   text-align: center;
   color: #555;
 `
 
-export const NotificationPlugin: FC<MessageComponentProps> = props => {
+export const NotificationPlugin: FC<MessageComponentProps> = memo(props => {
   const { message } = props
 
   if (!message || !message.data) {
@@ -17,4 +18,4 @@ export const NotificationPlugin: FC<MessageComponentProps> = props => {
   return (
     <Notification>{message.data}</Notification>
   )
-}
+})
--- src/components/Message/plugins/Text.tsx
+++ src/components/Message/plugins/Text.tsx
@@ -1,19 +1,20 @@
-import {FC} from 'react'
+import {FC, memo} from 'react'
 import {MessageComponentProps} from '../../../typing'
 import {Bubble} from '../Bubble'
 import {MessageBox} from '../MessageBox'
 
-export const TextPlugin: FC<MessageComponentProps> = props => {
-  const { message } = props
+export const TextPlugin: FC<MessageComponentProps> = memo(props => {
+    const { message } = props
 
-  if (!message || !message.data) {
-    return null
+    if (!message || !message.data) {
+      return null
+    }
+
+    return (
+      <MessageBox
+        author={message.author}
+        bubbleRender={() => <Bubble>{message.data}</Bubble>}
+      />
+    )
   }
-
-  return (
-    <MessageBox
-      author={message.author}
-      bubbleRender={() => <Bubble>{message.data}</Bubble>}
-    />
-  )
-}
+)
